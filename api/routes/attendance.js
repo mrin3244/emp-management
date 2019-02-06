2b6811d7cf71a50a2debbfbcafe2fb540751c2e4
Mrinmoy Chowdhury <mrinmoy.btkol@gmail.com>
2019-02-06 14:58:07 +0530

    attendance late time calculate

--- api/routes/attendance.js
+++ api/routes/attendance.js
@@ -17,6 +17,24 @@ module.exports = function(dbs){
             var time = moment(dt).format('HH:mm:ss');
 
             const eid = mongodb.ObjectID(sess.eid);
+
+            //var starttime = moment("09:45 AM", 'hh:mm A');
+            //var starttime = moment("07:30 PM", 'hh:mm A');
+            // console.log(starttime.from(dt));
+            // console.log(moment(dt).from(starttime));
+            // console.log(time);
+            // var intimediff = '';
+            // var instatus = '';
+            // var checkin = new Date();
+            // if(moment.min(checkin,starttime) == checkin){
+            //     intimediff = moment.utc(moment(starttime,"DD/MM/YYYY HH:mm:ss").diff(moment(checkin,"DD/MM/YYYY HH:mm:ss"))).format("HH:mm:ss");
+            //     instatus = 'early';
+            // }else {
+            //     intimediff = moment.utc(moment(checkin,"DD/MM/YYYY HH:mm:ss").diff(moment(starttime,"DD/MM/YYYY HH:mm:ss"))).format("HH:mm:ss");
+            //     instatus = 'late';
+            // }
+            // console.log('you are '+ instatus +' at time '+ intimediff);
+
             var start = new Date();
             start.setHours(0,0,0,0);
 
@@ -41,7 +59,12 @@ module.exports = function(dbs){
                         ename: sess.empdata[0].name,
                         imgname: sess.empdata[0].imgname,
                         checkin: doc.checkin,
-                        checkout : doc.checkout
+                        checkout : doc.checkout,
+                        intimediff: doc.intimediff,
+                        instatus: doc.instatus,
+                        outtimediff: doc.outtimediff,
+                        outstatus: doc.outstatus
+
                     });
                 } else {
                     res.render('pages/put_attendance', { 
@@ -67,53 +90,100 @@ module.exports = function(dbs){
         sess = req.session;
         if(sess.eid){
             const eid = mongodb.ObjectID(sess.eid);
-            // for insert new day data
-            if(req.body.checkoption=="in"){
-                const dataset = {
-                    empid: eid,
-                    checkin: new Date()
-                };
-                dbs.collection('attendance').insertOne(dataset, function(err,result){
-                    if(result){
-                        return res.redirect('/home');
-                    } 
-                    if(err){
-                        return res.render('pages/500', {
-                            eid:sess.eid, 
-                            utypeid:sess.utypeid,
-                            ename: sess.empdata[0].name,
-                            imgname: sess.empdata[0].imgname, 
-                            message: err
+            dbs.collection('rules').findOne({}, {endtime:1, starttime:1, _id:0}, function(err, doc){
+                if(err){
+                    return res.render('pages/500', {
+                        eid:sess.eid, 
+                        utypeid:sess.utypeid,
+                        ename: sess.empdata[0].name, 
+                        imgname: sess.empdata[0].imgname,
+                        message: err
+                    });
+                }
+                if(doc){
+                    var starttime = doc.starttime;
+                    var endtime = doc.endtime;
+                    // for insert new day data
+                    if(req.body.checkoption=="in"){
+                        console.log(starttime);
+                        var starttime = moment(starttime, 'hh:mm A');
+                        console.log(starttime);
+                        var intimediff = '';
+                        var instatus = '';
+                        var checkin = new Date();
+                        if(moment.min(checkin,starttime) == checkin){
+                            intimediff = moment.utc(moment(starttime,"DD/MM/YYYY HH:mm:ss").diff(moment(checkin,"DD/MM/YYYY HH:mm:ss"))).format("HH:mm:ss");
+                            instatus = 'early';
+                        }else {
+                            intimediff = moment.utc(moment(checkin,"DD/MM/YYYY HH:mm:ss").diff(moment(starttime,"DD/MM/YYYY HH:mm:ss"))).format("HH:mm:ss");
+                            instatus = 'late';
+                        }
+                        console.log('you are '+ instatus +' at time '+ intimediff);
+                        const dataset = {
+                            empid: eid,
+                            checkin: checkin,
+                            intimediff: intimediff,
+                            instatus: instatus
+                        };
+                        console.log(dataset);
+                        dbs.collection('attendance').insertOne(dataset, function(err,result){
+                            if(result){
+                                return res.redirect('/home');
+                            } 
+                            if(err){
+                                return res.render('pages/500', {
+                                    eid:sess.eid, 
+                                    utypeid:sess.utypeid,
+                                    ename: sess.empdata[0].name,
+                                    imgname: sess.empdata[0].imgname, 
+                                    message: err
+                                });
+                            }
                         });
                     }
-                });
-            }
-            // for update the out time
-            if(req.body.checkoption=="out"){
-                const dataset = {
-                    checkout: new Date()
-                };
-                var start = new Date();
-                start.setHours(0,0,0,0);
-                var end = new Date();
-                end.setHours(23,59,59,999);
-                dbs.collection('attendance').findOneAndUpdate({"empid":eid, "checkin": {$gte: start, $lt: end}}, {$set: dataset}, function(err,result){
-                    if(result){
-                        //console.log(result);
-                        return res.redirect('/home');
-                    } 
-                    if(err){
-                        return res.render('pages/500', {
-                            eid:sess.eid, 
-                            utypeid:sess.utypeid,
-                            ename: sess.empdata[0].name,
-                            imgname: sess.empdata[0].imgname, 
-                            message: err
+                    // for update the out time
+                    if(req.body.checkoption=="out"){
+                        var endtime = moment(endtime, 'hh:mm A');
+                        var outtimediff = '';
+                        var outstatus = '';
+                        var checkout = new Date();
+                        if(moment.min(checkout,endtime) == checkout){
+                            outtimediff = moment.utc(moment(endtime,"DD/MM/YYYY HH:mm:ss").diff(moment(checkout,"DD/MM/YYYY HH:mm:ss"))).format("HH:mm:ss");
+                            outstatus = 'early';
+                        }else {
+                            outtimediff = moment.utc(moment(checkout,"DD/MM/YYYY HH:mm:ss").diff(moment(endtime,"DD/MM/YYYY HH:mm:ss"))).format("HH:mm:ss");
+                            outstatus = 'over';
+                        }
+                        //console.log('you are '+ instatus +' at time '+ intimediff);
+                        const dataset = {
+                            checkout: checkout,
+                            outtimediff: outtimediff,
+                            outstatus: outstatus 
+                        };
+                        var start = new Date();
+                        start.setHours(0,0,0,0);
+                        var end = new Date();
+                        end.setHours(23,59,59,999);
+                        dbs.collection('attendance').findOneAndUpdate({"empid":eid, "checkin": {$gte: start, $lt: end}}, {$set: dataset}, function(err,result){
+                            if(result){
+                                //console.log(result);
+                                return res.redirect('/home');
+                            } 
+                            if(err){
+                                return res.render('pages/500', {
+                                    eid:sess.eid, 
+                                    utypeid:sess.utypeid,
+                                    ename: sess.empdata[0].name,
+                                    imgname: sess.empdata[0].imgname, 
+                                    message: err
+                                });
+                            }
                         });
+                        
                     }
-                });
-                
-            }
+                }
+            });
+            
         }
         else{
             return res.redirect('/err?message=Auth failed');
@@ -142,18 +212,18 @@ module.exports = function(dbs){
                 } 
                 if(docs){
                     //console.log(docs);
-                    var days;
-                    var intime;
-                    var outtime;
-                    var workhours;
-                    var history=[];
-                    var employee=[];
+                    var days; var intime; var outtime; var workhours; var instatus; var intimediff;
+                    var outstatus; var outtimediff; var history=[]; var employee=[];
                     for(i=0; i<docs.length; i++){
                         days = moment(docs[i].checkin).format('DD/MM/YYYY');
                         intime = moment(docs[i].checkin).format('HH:mm:ss');
-                        outtime = moment(docs[i].checkout).format('HH:mm:ss')
-                        workhours = moment.utc(moment(docs[i].checkout,"DD/MM/YYYY HH:mm:ss").diff(moment(docs[i].checkin,"DD/MM/YYYY HH:mm:ss"))).format("HH:mm:ss")
-                        employee={days:days,intime: intime, outtime: outtime, workhours: workhours}
+                        instatus = docs[i].instatus;
+                        intimediff = docs[i].intimediff;
+                        outtime = moment(docs[i].checkout).format('HH:mm:ss');
+                        outstatus = docs[i].outstatus;
+                        outtimediff = docs[i].outtimediff;
+                        workhours = moment.utc(moment(docs[i].checkout,"DD/MM/YYYY HH:mm:ss").diff(moment(docs[i].checkin,"DD/MM/YYYY HH:mm:ss"))).format("HH:mm:ss");
+                        employee={days:days,intime: intime, instatus: instatus, intimediff: intimediff, outtime: outtime, outstatus: outstatus, outtimediff: outtimediff, workhours: workhours}
                        history.push(employee);
                     }
                     
@@ -199,18 +269,18 @@ module.exports = function(dbs){
                 } 
                 if(docs){
                     //console.log(docs);
-                    var days;
-                    var intime;
-                    var outtime;
-                    var workhours;
-                    var history=[];
-                    var employee=[];
+                    var days; var intime; var outtime; var workhours; var instatus; var intimediff;
+                    var outstatus; var outtimediff; var history=[]; var employee=[];
                     for(i=0; i<docs.length; i++){
                         days = moment(docs[i].checkin).format('DD/MM/YYYY');
                         intime = moment(docs[i].checkin).format('HH:mm:ss');
-                        outtime = moment(docs[i].checkout).format('HH:mm:ss')
-                        workhours = moment.utc(moment(docs[i].checkout,"DD/MM/YYYY HH:mm:ss").diff(moment(docs[i].checkin,"DD/MM/YYYY HH:mm:ss"))).format("HH:mm:ss")
-                        employee={days:days,intime: intime, outtime: outtime, workhours: workhours}
+                        instatus = docs[i].instatus;
+                        intimediff = docs[i].intimediff;
+                        outtime = moment(docs[i].checkout).format('HH:mm:ss');
+                        outstatus = docs[i].outstatus;
+                        outtimediff = docs[i].outtimediff;
+                        workhours = moment.utc(moment(docs[i].checkout,"DD/MM/YYYY HH:mm:ss").diff(moment(docs[i].checkin,"DD/MM/YYYY HH:mm:ss"))).format("HH:mm:ss");
+                        employee={days:days,intime: intime, instatus: instatus, intimediff: intimediff, outtime: outtime, outstatus: outstatus, outtimediff: outtimediff, workhours: workhours}
                        history.push(employee);
                     }
                     
